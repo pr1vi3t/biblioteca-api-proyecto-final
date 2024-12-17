f1e6557cd3de3fa21769ef619f92ed44185e7622
xlrandylx <jrf_jose@hotmail.com>
2024-12-16 19:44:58 -0500

    feat: añadiendo services, controllers, routes de autor

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, {Application, Request, Response} from 'express';
 import morgan from 'morgan';
 import lectorRouter from './routes/lector.route'
 import editorialRouter from './routes/editorial.route'
+import autorRouter from './routes/autor.route'
 
 const app: Application = express();
 
@@ -9,5 +10,6 @@ app.use(morgan('dev'));
 
 app.use('/api/v1/lectores',lectorRouter);
 app.use('/api/v1/editoriales',editorialRouter);
+app.use('/api/v1/autores',autorRouter);
 
 export default app;
